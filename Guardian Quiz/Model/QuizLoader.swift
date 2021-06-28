dc60ce93960d5ab830d88e49892a3c5ee040ace9
Simon Whitaker <sw@netcetera.org>
2021-06-28 19:58:47 +0100

    Disable local HTTP cache

--- Guardian Quiz/Model/QuizLoader.swift
+++ Guardian Quiz/Model/QuizLoader.swift
@@ -30,10 +30,7 @@ typealias LoadQuizCallback = (QuizResultType) -> Void
 func loadQuizFromURL(url: URL, callback: @escaping LoadQuizCallback) {
   let session = URLSession(configuration: .default)
 
-  // Remove cache entries older than 4 hours
-  session.configuration.urlCache?.removeCachedResponses(since: Date(timeIntervalSinceNow: -4*60*60))
-
-  let request = URLRequest(url: url, cachePolicy: .returnCacheDataElseLoad, timeoutInterval: 10.0)
+  let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10.0)
   let dataTask = session.dataTask(with: request) { data, response, error in
     if let error = error {
       callback(.failure(.unknownError(underlyingError: error)))
