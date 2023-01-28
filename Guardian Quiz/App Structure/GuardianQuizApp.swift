a32bb9c3a638c3c448b5baf8f2c05c9c80ee6221
Simon Whitaker <sw@netcetera.org>
2023-01-28 12:24:56 +0000

    feat: add external window support
    https://prathamesh.xyz/blog/2020/10/7/add-multi-screen-support-to-swiftui-apps
    https://github.com/p16r/Screens

--- Guardian Quiz/App Structure/GuardianQuizApp.swift
+++ Guardian Quiz/App Structure/GuardianQuizApp.swift
@@ -6,15 +6,63 @@
 //
 
 import SwiftUI
+import Combine
 
 @main
 struct GuardianQuizApp: App {
-    @UIApplicationDelegateAdaptor private var appDelegate: AppDelegate
     @StateObject private var sharedState: SharedState = SharedState()
+    @State var additionalWindows: [UIWindow] = []
+
+    private var screenDidConnectPublisher: AnyPublisher<UIScreen, Never> {
+        NotificationCenter.default
+            .publisher(for: UIScreen.didConnectNotification)
+            .compactMap { $0.object as? UIScreen }
+            .receive(on: RunLoop.main)
+            .eraseToAnyPublisher()
+    }
+
+    private var screenDidDisconnectPublisher: AnyPublisher<UIScreen, Never> {
+        NotificationCenter.default
+            .publisher(for: UIScreen.didDisconnectNotification)
+            .compactMap { $0.object as? UIScreen }
+            .receive(on: RunLoop.main)
+            .eraseToAnyPublisher()
+    }
 
     var body: some Scene {
         WindowGroup {
-            QuizmasterView().environmentObject(sharedState)
+            QuizmasterView()
+                .environmentObject(sharedState)
+                .onReceive(
+                    screenDidConnectPublisher,
+                    perform: screenDidConnect
+                )
+                .onReceive(
+                    screenDidDisconnectPublisher,
+                    perform: screenDidDisconnect
+                )
         }
+    }
+
+    private func screenDidConnect(_ screen: UIScreen) {
+        let window = UIWindow(frame: screen.bounds)
+
+        window.windowScene = UIApplication.shared.connectedScenes
+            .first { ($0 as? UIWindowScene)?.screen == screen }
+        as? UIWindowScene
+
+        let view = PlayerView()
+            .environmentObject(sharedState)
+        let controller = UIHostingController(rootView: view)
+        window.rootViewController = controller
+        window.isHidden = false
+        additionalWindows.append(window)
+
+        sharedState.isSecondScreenVisible = true
+    }
+
+    private func screenDidDisconnect(_ screen: UIScreen) {
+        additionalWindows.removeAll { $0.screen == screen }
+        sharedState.isSecondScreenVisible = false
     }
 }
